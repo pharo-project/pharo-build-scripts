ebe9ef5bff10f4513845a2281ee113aa8d79cffd
Igor Stasenko <siguctua@gmail.com>
2011-07-10 18:36:04 +0200

    another fix for paths and quotes

--- pharo-shell-scripts/fetchPharo14.sh
+++ pharo-shell-scripts/fetchPharo14.sh
@@ -7,8 +7,8 @@ mkdir -p $WORKSPACE/images
 sh "$WORKSPACE/download.sh" "$WORKSPACE/images/Pharo14.zip" https://gforge.inria.fr/frs/download.php/28710/Pharo-1.4-14025.zip
 
 unzip -j -o -d "$WORKSPACE/images/" "$WORKSPACE/images/Pharo14.zip"
-mv "$WORKSPACE/images/Pharo-1.4*.image" "$WORKSPACE/images/gforge14.image"
-mv "$WORKSPACE/images/Pharo-1.4*.changes" "$WORKSPACE/images/gforge14.changes"
+mv "$WORKSPACE"/images/Pharo-1.4*.image "$WORKSPACE/images/gforge14.image"
+mv "$WORKSPACE"/images/Pharo-1.4*.changes "$WORKSPACE/images/gforge14.changes"
 
 # fetch PharoV10.sources
 
