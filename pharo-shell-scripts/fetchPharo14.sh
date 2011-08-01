e22053e8e4652d95e373a67fe6f078d9dc598ddc
Marcus Denker <marcus.denker@inria.fr>
2011-08-01 13:18:04 +0200

    update to load latest gforge 1.4

--- pharo-shell-scripts/fetchPharo14.sh
+++ pharo-shell-scripts/fetchPharo14.sh
@@ -4,7 +4,7 @@ fi
 
 mkdir -p "$WORKSPACE/images"
 
-sh "$WORKSPACE/download.sh" "$WORKSPACE/images/Pharo14.zip" https://gforge.inria.fr/frs/download.php/28710/Pharo-1.4-14025.zip
+sh "$WORKSPACE/download.sh" "$WORKSPACE/images/Pharo14.zip" https://gforge.inria.fr/frs/download.php/28830/Pharo-1.4-14066.zip 
 
 unzip -j -o -d "$WORKSPACE/images/" "$WORKSPACE/images/Pharo14.zip"
 mv "$WORKSPACE"/images/Pharo-1.4*.image "$WORKSPACE/images/gforge14.image"
