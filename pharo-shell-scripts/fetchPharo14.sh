ca254f24a7de5b20c6bef14e84627289d682a700
Marcus Denker <marcus.denker@inria.fr>
2012-02-27 16:39:27 +0100

    fetch pharo now fetches the last successfull build

--- pharo-shell-scripts/fetchPharo14.sh
+++ pharo-shell-scripts/fetchPharo14.sh
@@ -4,7 +4,7 @@ fi
 
 mkdir -p "$WORKSPACE/images"
 
-sh "$WORKSPACE/download.sh" "$WORKSPACE/images/Pharo14.zip" https://gforge.inria.fr/frs/download.php/30286/Pharo-1.4-14363.zip
+sh "$WORKSPACE/download.sh" "$WORKSPACE/images/Pharo14.zip" https://ci.lille.inria.fr/pharo/job/Pharo%201.4/lastSuccessfulBuild/artifact/Pharo-1.4.zip
 
 unzip -j -o -d "$WORKSPACE/images/" "$WORKSPACE/images/Pharo14.zip"
 mv "$WORKSPACE"/images/Pharo-1.4*.image "$WORKSPACE/images/gforge14.image"
