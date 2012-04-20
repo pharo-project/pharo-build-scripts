41547f3443db0627e06ec3c70469dc478aab4936
Marcus Denker <marcus.denker@inria.fr>
2012-04-20 17:46:40 +0200

    fetch script for 2.0

---
+++ pharo-shell-scripts/fetchPharo20.sh
@@ -0,0 +1,18 @@
+if [ -z "$WORKSPACE" ] ; then
+	WORKSPACE=.
+fi
+
+mkdir -p "$WORKSPACE/images"
+
+sh "$WORKSPACE/download.sh" "$WORKSPACE/images/Pharo20.zip" https://gforge.inria.fr/frs/download.php/30625/Pharo-2.0-007.zip
+
+unzip -j -o -d "$WORKSPACE/images/" "$WORKSPACE/images/Pharo20.zip"
+mv "$WORKSPACE"/images/Pharo-1.4*.image "$WORKSPACE/images/gforge20.image"
+mv "$WORKSPACE"/images/Pharo-1.4*.changes "$WORKSPACE/images/gforge20.changes"
+
+# fetch PharoV10.sources
+
+mkdir -p "$WORKSPACE/sources"
+sh "$WORKSPACE/download.sh" "$WORKSPACE/sources/sources.zip" https://gforge.inria.fr/frs/download.php/24391/PharoV10.sources.zip
+
+unzip -j -o -d "$WORKSPACE/sources/" "$WORKSPACE/sources/sources.zip"
