f5f7d10bd8d599805cc4fc0d57cf2acf2e9c953b
Marcus Denker <marcus.denker@inria.fr>
2012-07-07 20:47:07 +0200

    dl old 1.4 for types

---
+++ pharo-shell-scripts/fetchPharo14ForTypes.sh
@@ -0,0 +1,18 @@
+if [ -z "$WORKSPACE" ] ; then
+	WORKSPACE=.
+fi
+
+mkdir -p "$WORKSPACE/images"
+
+sh "$WORKSPACE/download.sh" "$WORKSPACE/images/Pharo14.zip" https://gforge.inria.fr/frs/download.php/30620/Pharo-1.4-14438.zip
+
+unzip -j -o -d "$WORKSPACE/images/" "$WORKSPACE/images/Pharo14.zip"
+mv "$WORKSPACE"/images/Pharo-1.4*.image "$WORKSPACE/images/gforge14.image"
+mv "$WORKSPACE"/images/Pharo-1.4*.changes "$WORKSPACE/images/gforge14.changes"
+
+# fetch PharoV10.sources
+
+mkdir -p "$WORKSPACE/sources"
+sh "$WORKSPACE/download.sh" "$WORKSPACE/sources/sources.zip" https://gforge.inria.fr/frs/download.php/24391/PharoV10.sources.zip
+
+unzip -j -o -d "$WORKSPACE/sources/" "$WORKSPACE/sources/sources.zip"
