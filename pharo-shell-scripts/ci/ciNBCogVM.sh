2a42241f0284cec06bbd0a9fbc98fffa3e3ff35a
Camillo Bruni <camillobruni@gmail.com>
2013-02-05 11:59:56 +0100

    download sources files into proper directory

--- pharo-shell-scripts/ci/ciNBCogVM.sh
+++ pharo-shell-scripts/ci/ciNBCogVM.sh
@@ -55,7 +55,7 @@ echo $PHARO_VM
 # DOWNLOAD THE PharoV10.sources ===============================================
 SOURCES_URL="http://pharo.gforge.inria.fr/ci/image/PharoV10.sources.zip"
 wget --progress=bar:force --output-document=sources.zip $SOURCES_URL
-unzip -qo -d vm sources.zip
+unzip -qo -d `dirname $PHARO_VM` sources.zip
 rm -rf sources.zip
 
 # create a local executable file which forwads to the found vm
