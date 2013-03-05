706291cec8a048e1382711a9116a2701015590dd
Camillo Bruni <camillobruni@gmail.com>
2013-03-05 15:22:38 +0100

    removed oboslete zerconf script file

--- pharo-shell-scripts/ci/ciPharo20NBCog.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# stop the script if a single command fails
-set -e 
-
-
-# OUTDATE! use newer script ===============================================
-wget --quiet -O - http://files.pharo.org/script/ciPharo20NBCogVM.sh | bash
