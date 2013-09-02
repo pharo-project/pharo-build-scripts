e5dca8c0a0353e3de240d2bd09242c687199773c
Camillo Bruni <camillobruni@gmail.com>
2013-09-02 18:50:07 -0300

    added line end conversion script

---
+++ pharo-shell-scripts/mac2unix.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# ARGUMENT HANDLING =============================================================
+if { [ "$1" = "-h" ] || [ "$1" = "--help" ]; }; then
+	echo 'This script converts newline endings in from \r (carriage return) to \n (line feed).
+Note that this script will overwrite files.
+	
+Usage:
+	mac2unix.sh path/to/file/withMacLineEndings.txt'
+	exit 1
+fi
+
+cat $1 | tr '\r' '\n' > $1
