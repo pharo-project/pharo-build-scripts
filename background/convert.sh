a24d59b1c9fe2719dcb3dd6a333c1cff2f74787a
Damien Cassou <damien.cassou@gmail.com>
2013-11-05 11:10:33 +0100

    Make sure that the background looks ok

--- background/convert.sh
+++ background/convert.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-convert background.png -flatten -alpha Off background-2.png
-mv background-2.png background.png
+# Make sure that the png will look good on Mac OS X when set as a
+# folder background
+convert background.png -flatten -alpha Off -density 28.35x28.35 \
+    -units PixelsPerCentimeter background.temp.png
+
+mv background.temp.png background.png
