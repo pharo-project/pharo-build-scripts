989a02c4d13a62aaaef783ec23093f85022abefe
Christophe Demarey <demarey@MacBook-Pro-de-demarey-2.local>
2017-09-29 16:00:51 +0200

    increase DMG size to avoid "No space left" error when trying to copy into the created Volume

--- build-dmg.sh
+++ build-dmg.sh
@@ -53,7 +53,7 @@ cp -rpf "${APP_NAME}.app" "${STAGING_DIR}"
 # figure out how big our DMG needs to be
 #  assumes our contents are at least 1M!
 SIZE=`du -sh "${STAGING_DIR}" | sed 's/\([0-9\.]*\)M\(.*\)/\1/'` 
-SIZE=`echo "${SIZE} + 1.0" | bc | awk '{print int($1+0.5)}'`
+SIZE=`echo "${SIZE} + 2.0" | bc | awk '{print int($1+0.5)}'`
 
 if [ $? -ne 0 ]; then
    echo "Error: Cannot compute size of staging dir"
